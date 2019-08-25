90c865079cc96bcda91afd4c687b3b85f07ed950
Nilomiranda <nilomiranda3@gmail.com>
2019-08-25 13:18:14 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import './config/reactotronConfig';
+
+console.tron.log('Hello');
+
+const App = () => {
+  return <View />;
+};
+
+export default App;
